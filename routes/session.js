5d3e85549ae5b43d12f306d61bdac39bf5af14cc
stevenvegt <steven.vandervegt@nedap.com>
2019-08-19 15:44:14 +0200

    Handle session post

--- routes/session.js
+++ routes/session.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const { nutsAuthClient } = require('@nuts-foundation/auth');
 
 router.get('/login', function (req, res, next) {
   res.render('login', {})
+});
+
+router.post('/login', async function (req, res, next) {
+  const nutsToken = req.body.nuts_auth_token
+
+  const client = nutsAuthClient('http://localhost:11323', 'Demo EHR');
+  const validationResponse = await client.validateToken(nutsToken);
+
+  const sessionIsValid = validationResponse.validation_result === "VALID";
+
+  if (!sessionIsValid) {
+    res.status(403).send("token invalid");
+    return;
+  }
+
+  // Store token in session information for one hour
+  // Note: the cookie should be signed in production environments
+  res.cookie('nutstoken', nutsToken, {maxAge: 60 * 60 * 1000});
+  res.cookie('agb', validationResponse.signer_attributes['irma-demo.nuts.agb.agbcode'], {maxAge: 60 * 60 * 1000});
+
+  res.status(200).send(validationResponse);
 });
 
 module.exports = router;
