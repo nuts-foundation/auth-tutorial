45b3193ce66ba4b4495e098a0eab7bc6e64503af
stevenvegt <steven.vandervegt@nedap.com>
2019-08-19 15:44:28 +0200

    Adda user page

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.all('/', function(req, res, next) {
+  // check if the cookie is set
+  if ('nutstoken' in req.cookies) {
+    next();
+  } else {
+    res.redirect('/session/login');
+  }
 });
 
+// render the user page with the agb code
+router.get('/', function (req, res, next) {
+  const agb = req.cookies.agb;
+  res.render('user', {agb});
+});
 module.exports = router;
